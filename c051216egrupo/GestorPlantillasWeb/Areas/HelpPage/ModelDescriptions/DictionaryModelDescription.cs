d99cc78767e6fe126e86e8a45ed945a5390ce019
Saul Pacheco Gonzalez <cloud_saul@hotmail.com>
2016-09-26 16:34:11 +0200

    Creacion del Proyecto WEB

---
+++ c051216egrupo/GestorPlantillasWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GestorPlantillasWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ c051216egrupo/GestorPlantillasWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GestorPlantillasWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
