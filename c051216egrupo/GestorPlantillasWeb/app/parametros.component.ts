16261994730d819668cd34291b02edf1de06d181
nacho <nacho>
2016-09-27 16:04:21 +0200

    añadidas clsaes de parametros sin realizar al completo

---
+++ c051216egrupo/GestorPlantillasWeb/app/parametros.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+
+import { Parametro }                from './modelos/parametro';
+import { ParametroService }         from './parametro.service';
+
+@Component({
+    selector: 'mis-parametros',
+    templateUrl: 'app/parametro.component.html',
+    styleUrls: ['app/parametro.component.css']
+})
+export class ParametrosComponent implements OnInit {
+    parametros: Parametro[];
+    selectedParametro: Parametro;
+
+  constructor(
+    private parametroService: ParametroService,
+    private router: Router) { }
+
+  getParametros(): void {
+      this.parametroService
+          .getParametros()
+          .then(parametros => this.parametros = parametros);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.parametroService.create(name)
+        .then(parametro => {
+            this.parametros.push(parametro);
+        this.selectedParametro = null;
+      });
+  }
+
+  delete(parametro: Parametro): void {
+      this.parametroService
+          .delete(parametro.id_parametro)
+        .then(() => {
+            this.parametros = this.parametros.filter(p => p !== parametro);
+            if (this.selectedParametro === parametro) { this.selectedParametro = null; }
+        });
+  }
+
+  ngOnInit(): void {
+      this.getParametros();
+  }
+
+  onSelect(parametro: Parametro): void {
+      this.selectedParametro = parametro;
+  }
+
+  gotoDetail(): void {
+      this.router.navigate(['/detail', this.selectedParametro.id_parametro]);
+  }
+}
+
+
