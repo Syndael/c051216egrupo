529f4c5ebca34f5cadede2e26bcbfd59dc2fa7c6
curso05 <curso05cic@gmail.com>
2016-09-27 18:07:26 +0200

    Commit

--- c051216egrupo/ProyectoTests/Class1Tests.cs
+++ c051216egrupo/ProyectoTests/Class1Tests.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using GestorPlantilla.Service;
+using GestorPlantillas;
+using GestorPlantillas.EntityVO;
+using GestorPlantillas.Repository;
+using GestorPlantillas.Utility;
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Proyecto;
 using System;
 using System.Collections.Generic;
@@ -11,10 +17,32 @@ namespace Proyecto.Tests
     [TestClass()]
     public class Class1Tests
     {
+        private IPlantillaService service;
+
+        [TestInitialize()]
+        public void TestSetUp()
+        {
+            var container = new UnityContainer();
+            container.RegisterInstance<IPlantillaUtility>(new PlantillaUtility());
+            container.RegisterType<IPlantillaRepository, PlantillaRepository>();
+            container.RegisterType<IPlantillaService, PlantillaService>();
+
+            service = container.Resolve<IPlantillaService>();
+        }
+
         [TestMethod()]
         public void Class1Test()
         {
-            Assert.Fail();
+            PlantillaVO plantillaVO = new PlantillaVO();
+
+            plantillaVO.nombre = " prueba";
+
+
+            service.post(plantillaVO);
+
+
+
+            Assert.AreEqual(true, true);
         }
     }
 }
